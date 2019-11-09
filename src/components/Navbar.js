126497591ad968a29faa2bd18e9dc96310e40295
Poomiphat <poomiphat.ch@indexlivingmall.com>
2019-11-09 23:27:32 +0700

    about

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,9 +64,9 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-end has-text-centered">
-              {/* <Link className="navbar-item" to="/about">
+              <Link className="navbar-item" to="/about">
                 About
-              </Link> */}
+              </Link>
               {/* <Link className="navbar-item" to="/products">
                 Products
               </Link> */}
